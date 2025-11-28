bce8bee923f4ff8634e67d9b7202578cffa17485
Guy Torbet <guy@torbet.co>
2025-11-28 18:13:52 +0000

    sensible config defaults

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,18 +1,23 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    jwt_secret: str
-    database_user: str
-    database_password: str
-    database_host: str
-    database_port: int
-    database_name: str
+class AuthSettings(BaseSettings):
+    jwt_secret: str = "secret"
+
+
+class DatabaseSettings(BaseSettings):
+    database_user: str = "admin"
+    database_password: str = "admin"
+    database_host: str = "localhost"
+    database_port: int = 5432
+    database_name: str = "db"
 
     @property
     def database_url(self) -> str:
         return f"postgresql+asyncpg://{self.database_user}:{self.database_password}@{self.database_host}:{self.database_port}/{self.database_name}"
 
+
+class Settings(AuthSettings, DatabaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
